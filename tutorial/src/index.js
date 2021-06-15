6211de257db197e32a7c32283f63425a6365dab6
AdityaPunetha <adityapunetha6@gmail.com>
2021-06-15 19:04:43 +0530

    Watched till 1:19 ( jsx )

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,18 +1,19 @@
-import React from "react"
-import ReactDom from "react-dom"
+import React from "react";
+import ReactDom from "react-dom";
 
 function Greeting() {
-    return {
-        <div>
-        < h4 >
-            Hello World
-        </h4 >;
+  return (
+    <div>
+      <h1>hello</h1>
     </div>
+  );
 }
-}
+// const Greeting = () => {
+//   return React.createElement(
+//     "h1",
+//     {},
+//     React.createElement("h1", {}, "hello w")
+//   );
+// };
 
-//const Greeting = () => {
-//    return React.createElement("h1", {}, "hello world")
-//};
-
-ReactDom.render(<Greeting />, document.getElementById('root'));
+ReactDom.render(<Greeting />, document.getElementById("root"));
