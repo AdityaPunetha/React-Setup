ff5e7bc4edaad6a2b551ff54308cd10f9bcaa4d7
AdityaPunetha <adityapunetha6@gmail.com>
2021-06-16 14:41:14 +0530

    till 2:48 proper list

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,38 +1,35 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
-const firstBook = {
-  title: "The Psychology of Money",
-  author: "Morgan Housel",
-  img: "https://m.media-amazon.com/images/I/81cpDaCJJCL._AC_UY218_.jpg",
-};
-const secondBook = {
-  title: "The Secret Garden",
-  author: "Frances Hodgson Burnett",
-  img: "https://m.media-amazon.com/images/I/81oZ2suDhdS._AC_UY218_.jpg",
-};
+const books = [
+  {
+    title: "The Psychology of Money",
+    author: "Morgan Housel",
+    img: "https://m.media-amazon.com/images/I/81cpDaCJJCL._AC_UY218_.jpg",
+  },
+  {
+    title: "The Secret Garden",
+    author: "Frances Hodgson Burnett",
+    img: "https://m.media-amazon.com/images/I/81oZ2suDhdS._AC_UY218_.jpg",
+  },
+];
+
 function Booklist() {
   return (
     <section className="booklist">
-      <Book
-        title={firstBook.title}
-        author={firstBook.author}
-        img={firstBook.img}
-      />
-      <Book
-        title={secondBook.title}
-        author={secondBook.author}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        const { img, title, author } = book;
+        return Book({ img, title, author });
+      })}
     </section>
   );
 }
-const Book = (props) => {
+const Book = ({ img, title, author }) => {
   return (
     <article className="book">
-      <img src={props.img} alt="" />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
